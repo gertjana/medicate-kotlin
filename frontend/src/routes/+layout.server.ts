940c90d8c69b97e02490e34da96d838f909f1298
Gertjan Assies <gertjan.assies@gmail.com>
2026-02-24 11:27:54 +0100

    fix: remove empty layout.server.ts that caused unknown prop warnings

--- frontend/src/routes/+layout.server.ts
+++
@@ -1,6 +0,0 @@
-import type { LayoutServerLoad } from './$types';
-
-export const load: LayoutServerLoad = async ({ locals }) => {
-  // Optionally, you can preload user info here if you have session/cookie auth
-  return {};
-};
